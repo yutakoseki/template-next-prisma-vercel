bde65d44c4673ea79fc126ab16a4e6983185483a
ksk <emailofyuta@gmail.com>
2023-11-18 04:25:19 +0900

    API修正

---
+++ app/user/addUser.tsx
@@ -0,0 +1,75 @@
+// input タグのonChangeを使うためにClient Componentにする
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function AddUser() {
+    const router = useRouter();
+
+    const [id, setId] = useState("");
+    const [name, setName] = useState("");
+
+    // Userテーブルへデータを書き込む
+    const fetchAsyncAddUser = async () => {
+        // 入力されていないものがあれば、登録しない
+        if (id == "" || name == "") {
+            alert("すべての項目を埋めてください");
+            return;
+        }
+
+        // APIのURL
+        const url = "https://next-prisma-vercel-pied.vercel.app/api/user";
+        // リクエストパラメータ
+        const params = {
+            method: "POST",
+            // JSON形式のデータのヘッダー
+            headers: {
+                "Content-Type": "application/json",
+            },
+            // リクエストボディ
+            body: JSON.stringify({
+                id: id,
+                name: name,
+            }),
+        };
+
+        // APIへのリクエスト
+        await fetch(url, params);
+
+        // 入力値を初期化
+        setId("");
+        setName("");
+
+        // 画面をリフレッシュ
+        router.refresh();
+    };
+
+    // inputタグのvalueに変化があった際に実行される
+    const changeIdInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setId(e.target.value);
+    };
+    // inputタグのvalueに変化があった際に実行される
+    const changeNameInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setName(e.target.value);
+    };
+
+    return (
+        <div>
+            <h2>Add User</h2>
+            <div>
+                <div>
+                    <label>Id:</label>
+                    <input type="text" name="id" value={id} onChange={changeIdInput} />
+                </div>
+                <div>
+                    <label>Name:</label>
+                    <input type="text" name="name" value={name} onChange={changeNameInput} />
+                </div>
+                <div>
+                    <button onClick={fetchAsyncAddUser}>追加</button>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ app/user/userList.tsx
@@ -0,0 +1,26 @@
+export default async function UserList() {
+    // APIのURL
+    const url = "https://next-prisma-vercel-pied.vercel.app/api/user";
+
+    // APIへリクエスト
+    const res = await fetch(url, {
+        cache: "no-store",
+    });
+    // レスポンスボディを取り出す
+    const data = await res.json();
+
+    return (
+        <div>
+            <h2>All Users</h2>
+            <br/>
+            {data.map((user: any, index: any) => (
+                <div key={index}>
+                    <div>Id: {user.id}</div>
+                    <div>Name: {user.name}</div><br/>
+                    {/* {user.posts.map((value: any) => `${value.title},`)} Profile:
+                    {user.profile?.bio} */}
+                </div>
+            ))}
+        </div>
+    );
+}
