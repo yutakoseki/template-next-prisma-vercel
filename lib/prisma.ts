44eddf1230c89adff93f92cca91358d2d9ec6833
ksk <emailofyuta@gmail.com>
2023-11-18 03:55:33 +0900

    修正

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,3 +1,20 @@
+// Prisma Clientのインポート
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+const prismaClientSingleton = () => {
+    return new PrismaClient({
+        log: ["query"],
+    });
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
