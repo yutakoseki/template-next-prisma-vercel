e6d06f519654df454b7c0e6ec41274f7c7ea6b4e
ksk <emailofyuta@gmail.com>
2023-11-18 16:53:26 +0900

    いったんデプロイするところまで

---
+++ app/user/page.tsx
@@ -0,0 +1,15 @@
+
+// import UserList from "./userList";
+import AddUser from "./addUser";
+import UserList from "./userList";
+
+export default async function Home() {
+  return (
+    <div>
+      <AddUser/>
+      <br />
+      {/* User一覧を表示するコンポーネント */}
+      <UserList />
+    </div>
+  );
+}
